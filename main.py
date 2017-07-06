e5baed74e9b46fedc1992a57e4342b1fadff01f2
Bill Phan <billphan25@gmail.com>
2017-07-05 22:25:03 -0700

    Linked to local SQL database and added functionality to delete items and show completed items.

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
+# Object relational mapping with Python, Flask, SQL, MySQLAlchemy
+
 from flask import Flask, request, redirect, render_template
+# importing sqlalchemy modules
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+# SQL database configurations. Server name:Server password:port/server name.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:getitdone@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-tasks = []
+# persistent task class
+class Task(db.Model):
+    # every persistent class needs an id
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
+    def __init__(self, name):
+        self.name = name
+        self.completed = False
+
+# index request handler
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    # if request is POST, coming from submitting the form.
+    # then grab that data and creates a new class
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    return render_template('todos.html', title = "Get It Done!", tasks = tasks)
+    tasks = Task.query.filter_by(completed = False).all()
+    completed_tasks = Task.query.filter_by(completed = True).all()
+    return render_template('todos.html', title = "Get It Done!",
+        tasks = tasks, completed_tasks = completed_tasks)
 
-app.run()
+#  delete task request handler
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
